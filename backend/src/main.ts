b17da7abb687bc0432c5d439fce68d5636c439b5
rogai460 <rogai.games.id@gmail.com>
2023-01-12 13:26:08 +0900

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,3 +10,35 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+// ========= 以下を追記 =========
+// Electronのモジュール
+const electron = require('electron');
+
+// アプリケーションをコントロールするモジュール
+const app = electron.app;
+
+// ウィンドウを作成するモジュール
+const BrowserWindow = electron.BrowserWindow;
+
+// メインウィンドウはGCされないようにグローバル宣言
+let mainWindow;
+
+// 全てのウィンドウが閉じたら終了
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// Electronの初期化完了後に実行
+app.on('ready', () => {
+  // メイン画面の表示。ウィンドウの幅、高さを指定できる
+  mainWindow = new BrowserWindow({ width: 1200, height: 720 });
+  mainWindow.loadURL('http://localhost:3000/');
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
